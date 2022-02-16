89c7b3e7a84e9505620e4573d86747f229bada31
aucuparia <aucuparia@users.noreply.github.com>
2022-02-16 10:33:21 +0100

    Code cosmetics

--- controllers/Datarequest.php
+++ controllers/Datarequest.php
@@ -502,12 +502,12 @@ class Datarequest extends MY_Controller
 
         # Upload the document
         $this->api->call('datarequest_signed_dta_upload_permission', ['request_id' => $requestId,
-                                                               'action' => 'grant'])->data;
+                                                                      'action' => 'grant'])->data;
         $this->filesystem->upload($rodsaccount, $filePath, $_FILES["file"]);
         $this->api->call('datarequest_signed_dta_post_upload_actions',
                          ['request_id' => $requestId, 'filename' => $_FILES["file"]["name"]]);
         $this->api->call('datarequest_signed_dta_upload_permission', ['request_id' => $requestId,
-                                                               'action' => 'grantread'])->data;
+                                                                      'action' => 'grantread'])->data;
     }
 
     public function download_signed_dta($requestId) {
